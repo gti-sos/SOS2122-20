4061fea81c8f11cb6ee97a723d705649a692d510
danpucjim <dpuchejimenez@gmail.com>
2022-03-12 11:19:03 +0100

    Created Static public and dynamic ascii faces at /cool

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cool = require("cool-ascii-faces");
+
+const port = process.env.port || 8080; 
+const app = express();
+var cool1 = cool();
+
+
+app.use("/",express.static('public'));
+app.get("/route2",(req,res)=>{
+    console.log("Requested / route")
+    res.send("<html><body>Hola</body></html>")
+});
+
+app.get("/cool",(req,res)=>{
+    console.log("Requested / route")
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+})
+
+app.listen(port,() => {
+    console.log(`Server Ready at : ${port}`);
+});
+
+console.log(`Server Ready  at : ${port}`);
