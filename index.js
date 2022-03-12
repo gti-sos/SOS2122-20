4e1bd4d9192afaac89771e228846aab31e3b49ef
Alejandro Jorge Poyuelo <Alejandro Jorge Poyuelo>
2022-03-12 12:14:31 +0100

    prueba servidor heroku

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express");
 const cool = require("cool-ascii-faces");
+const express = require("express");
 
-const port = process.env.port || 8081; 
-const app = express();
+const app=express();
+const port = process.env.PORT || 8080;
 
+app.use("/", express.static('public'));
 
-app.use("/",express.static('public'));
-app.get("/route2",(req,res)=>{
-    console.log("Requested / route");
-    res.send("<html><body>Hola</body></html>");
+app.get("/faces", (req,res)=>{
+    console.log("Requested / faces route");
+    res.send("<html><body><h1>"+cool()+"!</h1></body></html>")
 });
 
-app.get("/cool",(req,res)=>{
-    console.log("Requested / route");
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
-})
+app.get("/time", (req,res)=>{
+    console.log("Requested / time route");
+    res.send("<html><body><h1>"+cool()+"!</h1></body></html>")
+});
+
+
+app.listen(port, () => {
+    console.log(`Server TRULY ready at port ${port}`);
+});
