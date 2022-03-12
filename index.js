795faec712bed1b0520b26c9d502576e00e7c25e
danpucjim <dpuchejimenez@gmail.com>
2022-03-12 12:07:35 +0100

    error

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 
 app.use("/",express.static('public'));
 app.get("/route2",(req,res)=>{
-    console.log("Requested / route")
-    res.send("<html><body>Hola</body></html>")
+    console.log("Requested / route");
+    res.send("<html><body>Hola</body></html>");
 });
 
 app.get("/cool",(req,res)=>{
-    console.log("Requested / route")
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+    console.log("Requested / route");
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
 })
