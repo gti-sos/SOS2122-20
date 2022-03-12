625f0ca7c5348713001ef6a7d8f54211e8afe81c
danpucjim <dpuchejimenez@gmail.com>
2022-03-12 12:05:06 +0100

    error

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cool = require("cool-ascii-faces");
 
-const port = process.env.port; 
+const port = process.env.port || 8081; 
 const app = express();
-var cool1 = cool();
 
 
 app.use("/",express.static('public'));
@@ -16,9 +15,3 @@ app.get("/cool",(req,res)=>{
     console.log("Requested / route")
     res.send("<html><body><h1>"+cool()+"</h1></body></html>")
 })
-
-app.listen(port,() => {
-    console.log(`Server Ready at : ${port}`);
-});
-
-console.log(`Server Ready  at : ${port}`);
