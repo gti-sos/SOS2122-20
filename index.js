d785ecf5b87b550bffad76f9ea5ac1282586489f
Alejandro Jorge Poyuelo <Alejandro Jorge Poyuelo>
2022-03-19 13:13:39 +0100

    GET y POST feedback opc

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser =require("body-parser");
 
 const BASE_API_URL = "/api/v1";
 
 const app=express();
-const port = process.env.PORT || 8080;
+app.use(bodyParser.json());
+const port = process.env.PORT || 8081;
 
 app.use("/", express.static('public'));
 
@@ -22,3 +24,28 @@ app.get("/time", (req,res)=>{
 app.listen(port, () => {
     console.log(`Server TRULY ready at port ${port}`);
 });
+
+// ALEJANDRO
+var fertilizers =[
+    {
+        country:"afghanistan",
+        year:2017,
+        quantity:17.80
+
+    },
+    {
+        country:"africa",
+        year:2017,
+        quantity:15.09
+    }
+];
+app.get(BASE_API_URL+ "/fertilizers",(req,res)=>{
+    res.send(JSON.stringify(fertilizers,null,2)); 
+
+});
+
+
+app.post(BASE_API_URL+ "/fertilizers",(req,res)=>{
+    fertilizers.push(req.body);
+    res.sendStatus(201,"CREATED"); 
+}); 
