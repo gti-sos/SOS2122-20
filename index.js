423a1e0b04622af6ad0a82d88c9edf367b1ffd7e
danpucjim <dpuchejimenez@gmail.com>
2022-03-19 19:40:26 +0100

    puche api update

--- index.js
+++ index.js
@@ -49,3 +49,34 @@ app.post(BASE_API_URL+ "/fertilizers",(req,res)=>{
     fertilizers.push(req.body);
     res.sendStatus(201,"CREATED"); 
 }); 
+
+
+//Daniel Puche
+
+var landusage = [
+    {
+        country : "Spain",
+        code : "SPA",
+        year : 2015,
+        "built-area" :10 ,
+        "grazing-area" :10 ,
+        "cropland-area" : 10,
+    },
+    {
+        country : "Spain",
+        code : "SPA",
+        year : 2016,
+        "built-area" :10 ,
+        "grazing-area" :10 ,
+        "cropland-area" : 10,
+    }
+]
+
+app.get(BASE_API_URL+ "/landusage-stats",(req,res)=>{
+    res.send(JSON.stringify(landusage,null,2)); 
+});
+
+app.post(BASE_API_URL+ "/landusage-stats",(req,res)=>{
+    landusage.push(req.body);
+    res.sendStatus(201,"CREATED"); 
+}); 
