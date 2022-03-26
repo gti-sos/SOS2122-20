5fad25091e9e6dd65851919a109bb75c02ea7022
danpucjim <dpuchejimenez@gmail.com>
2022-03-26 01:09:30 +0100

    Correcion de error danpucjim landusage-stats.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
 const bodyParser =require("body-parser");
-const request = require("request");
 const path = require("path");
 const BASE_API_URL = "/api/v1";
 const app=express();
