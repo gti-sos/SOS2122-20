aac4cfa158233f7507aae867526be8898e4cafd5
danpucjim <dpuchejimenez@gmail.com>
2022-04-19 18:14:16 +0200

    cambios en limit y offset danpucjim

--- src/front/danpucjimFront/landusageTables.svelte
+++ src/front/danpucjimFront/landusageTables.svelte
@@ -2,9 +2,12 @@
 	import Table from "sveltestrap/src/Table.svelte";
     import {onMount} from 'svelte';
 	import{
-        Button,NavLink,NavItem,Nav
+        Button,NavLink,NavItem,Nav,Pagination,PaginationItem,PaginationLink
     } from 'sveltestrap';
-	let contacts = [];
+	import {range} from "./range.js"
+
+	let pais,anyo; // pais y anyo de la busqueda
+	let contacts = []; // lista de todos los contactos
 	let newContact ={
 		country:"",
 		code: "",
@@ -12,12 +15,25 @@
 		built_area:"",
 		cropland_area:"",
 		grazing_area:""
-	};
-	let loading = true;
+	}; // contacto a insertar 
+
+	let current_page = 1; // pagina actual
+	let last_page = 1;
+	let limit = 10; // limite de visualizacion
+	let offset = 0; // offset actual
+	let numDataPag = 0; // 
+	let maxpag = false; // pagina maxima alcanzada
+
+	let loading = true; // esta carganado
+	let search = false; // se ha buscado
+	let busqueda = {}; // objeto tras la busqueda
+
 	onMount(getContacts);
+
+	//Funciones 
 	async function getContacts(){
 		console.log("Fetching Contacts ... ");
-		const res =  await fetch("/api/v1/landusage-stats");
+		const res =  await fetch("/api/v1/landusage-stats" + "?limit="+limit+"&offset="+offset);
 		if(res.ok){
 		const data =await res.json();
 		contacts = data;
@@ -67,6 +83,32 @@
 		});
 
 	}
+
+	async function searchContact(country,year){
+		offset = 0;
+		const res = await fetch("api/v1/landusage-stats"+"/" + country + "/" + year);
+
+		if(res.ok){
+			console.log("Buscando data... ");
+			search = true;
+			const json =  await res.json();
+			busqueda = json;
+			console.log(busqueda);
+			console.log(search);
+		}
+
+	}
+
+	function changePage(page,offset){
+		console.log("Change page");
+		console.log("Page" + page + "offset" + offset);
+		last_page = Math.ceil(total/10);
+		if(page !== current_page){
+			current_offset = offset;
+			current_page = page;
+		}
+		getContacts();
+	}
 </script>
 <main>
 	<Nav class = "bg-light">
@@ -75,6 +117,12 @@
         </NavItem>
         <NavItem>
             <NavLink id="nav-info" href="/#/info" style="text-decoration:none">Info</NavLink>
+        </NavItem>
+		<NavItem>
+            <NavLink id="nav-info" href="/#/info" style="text-decoration:none">Eliminar Todo</NavLink>
+        </NavItem>
+		<NavItem>
+            <NavLink id="nav-info" href="/#/info" style="text-decoration:none" class="text-succcess">Iniciar Datos</NavLink>
         </NavItem>
     </Nav>
     {#await contacts}
@@ -98,17 +146,65 @@
 				</tr>
 				<tr>
 					<td>
-						<input bind:value="{newContact.country}">
+						<input bind:value="{pais}">
 					</td>
 					<td>
-						<input bind:value="{newContact.country}">
+						<input bind:value="{anyo}">
 					</td>
 					<td>
-						<Button outline color="primary" on:click="{insertContact}">Buscar</Button>
+						<Button outline color="primary" on:click="{searchContact(pais,anyo)}">Buscar</Button>
 					</td>
 				</tr>
+				
 			</thead>
 		</Table>
+		{#if search}
+				<Table bordered class = "w-50 mx-auto">
+					<tr>
+						<th>
+							Pais
+						</th>
+		
+						<th>
+							Codigo
+						</th>
+						<th>
+							Anyo
+						</th>
+						<th>
+							Area Construida
+						</th>
+						<th>
+							Area de Pasto
+						</th>
+						<th>
+							Area de cultivo
+						</th>
+					</tr>
+					<tr>
+						<td>
+							{busqueda.country}
+						</td>
+						<td>
+							{busqueda.code}
+						</td>
+						<td>
+							{busqueda.year}
+						</td>
+						<td>
+							{busqueda.built_area}
+						</td>
+						<td>
+							{busqueda.grazing_area}
+						</td>
+						<td>
+							{busqueda.cropland_area}
+						</td>
+
+					</tr>
+				</Table>
+					
+					{/if}
 	</div>
 	<Table bordered>
 		<thead>
@@ -172,9 +268,27 @@
 			{/each}
 		</tbody>
 	</Table>
-	{/await}
+	<Pagination>
+		<PaginationItem class = {current_page ===1 ? "disabled" : ""}>
+			<PaginationLink
+			previous
+			id = "pagination-back"
+			href="#/landusage-stats"
+			on:click={()=> changePage(current_page-1,offset-10)}/>
+		</PaginationItem>
+
+		{#each range(last_page,1) as page}
+		<PaginationItem class={current_page === page ? "active": ""}>
+			
+		</PaginationItem>
+		{/each}
+	</Pagination>
 	<Button color="danger" on:click="{deleteContacts}">Eliminar Todo</Button>
 	<Button color ="success" on:click="{iniData}">InitialData</Button>
+	{/await}
+
+	
+
 
 </main>
 
